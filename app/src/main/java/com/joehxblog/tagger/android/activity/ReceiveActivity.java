da59d3ad221500d205d1eb187f42c03638a64e45
hendrixjoseph <hendixjoseph@aol.com>
2020-07-27 11:15:33 -0400

    fix issue where it was tagging based on default tag not selected tag

--- app/src/main/java/com/joehxblog/tagger/android/activity/ReceiveActivity.java
+++ app/src/main/java/com/joehxblog/tagger/android/activity/ReceiveActivity.java
@@ -14,35 +14,39 @@ import com.joehxblog.tagger.android.AffiliatePreference;
 import com.joehxblog.tagger.android.IntentTagger;
 import com.joehxblog.tagger.databinding.ReceiveActivityBinding;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ReceiveActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ReceiveActivityBinding receiveBinding = DataBindingUtil.setContentView(this, R.layout.receive_activity);
-
-        final Intent receiveIntent = getIntent();
-
-        IntentTagger intentTagger = new IntentTagger(receiveIntent);
-
-        receiveBinding.setTagged(intentTagger);
-
-        AffiliatePreference prefs = new AffiliatePreference(this);
+        final IntentTagger intentTagger = initIntentTagger();
+        final Spinner spinner = initSpinner();
 
         final Button tagItButton = findViewById(R.id.tagItButton);
+        tagItButton.setOnClickListener(v -> intentTagger.send(this, spinner.getSelectedItem().toString()));
+    }
 
-        tagItButton.setOnClickListener(v -> intentTagger.send(this, prefs.getDefaultTag()));
+    private IntentTagger initIntentTagger() {
+        final ReceiveActivityBinding receiveBinding = DataBindingUtil.setContentView(this, R.layout.receive_activity);
 
-        Spinner spinner = findViewById(R.id.tag_spinner);
-        List<String> tags = new ArrayList<>(prefs.getTags());
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, tags);
+        final Intent receiveIntent = getIntent();
+        final IntentTagger intentTagger = new IntentTagger(receiveIntent);
+        receiveBinding.setTagged(intentTagger);
+
+        return intentTagger;
+    }
+
+    private Spinner initSpinner() {
+        final AffiliatePreference prefs = new AffiliatePreference(this);
+
+        final Spinner spinner = findViewById(R.id.tag_spinner);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, prefs.getTags());
         spinner.setAdapter(adapter);
-        spinner.getSelectedItem().toString();
-        int defaultPosition = adapter.getPosition(prefs.getDefaultTag());
+
+        final int defaultPosition = adapter.getPosition(prefs.getDefaultTag());
         spinner.setSelection(defaultPosition);
+
+        return spinner;
     }
 }
